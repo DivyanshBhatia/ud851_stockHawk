3ef67fcc88c2caefdf7072aa5fec2cc3cceef891
dnbhatia <dnbhatia@ebay.com>
2017-01-08 21:06:13 +0530

    collection widget for stock quotes

---
+++ app/src/main/java/com/udacity/stockhawk/ui/widget/DetailWidgetRemoteViewsService.java
@@ -0,0 +1,105 @@
+package com.udacity.stockhawk.ui.widget;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Build;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
+
+/**
+ * Created by dnbhatia on 1/8/2017.
+ */
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class DetailWidgetRemoteViewsService extends RemoteViewsService {
+    public final String LOG_TAG = DetailWidgetRemoteViewsService.class.getSimpleName();
+
+    //Projection Columns will be quote columns
+
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+                final long identityToken = Binder.clearCallingIdentity();
+                data = getContentResolver().query(Contract.Quote.URI,
+                        Contract.Quote.QUOTE_COLUMNS,
+                        null,
+                        null,
+                        Contract.Quote.COLUMN_SYMBOL + " ASC");
+                data.moveToFirst();
+                Log.v(DetailWidgetRemoteViewsService.class.getSimpleName(),data.getString(Contract.Quote.POSITION_HISTORY)+"\t"+data.getString(Contract.Quote.POSITION_SYMBOL));
+                Binder.restoreCallingIdentity(identityToken);
+
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.stock_widget_layout);
+                views.setTextViewText(R.id.symbol,"AAPL");
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return null;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position))
+                    return data.getLong(Contract.Quote.POSITION_ID);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
